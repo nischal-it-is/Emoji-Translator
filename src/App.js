98be6bb6de32882e2149542fa896b7b25cdd637f
Nischal Singh <84283501+nischal-it-is@users.noreply.github.com>
2021-09-13 15:10:47 +0530

    Handling Input done

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ var user = "Nischal";
 var count = 0;
 export default function App() {
   const [likeCounter, setLikeCounter] = useState(0);
+  const [userInput, setUserInput] = useState(0);
   function likeClickHandler() {
     count += 1;
     setLikeCounter(count);
   }
+  function inputHandler(event) {
+    setUserInput(event.target.value);
+  }
   return (
     <div className="App">
       <h1 style={{ color: "blue" }}>Welcome {user}</h1>
+      <input onChange={inputHandler}></input>
+      <div>{userInput}</div>
       <button onClick={likeClickHandler}>Like</button>
       <h2>{likeCounter}</h2>
     </div>
