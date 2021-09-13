7ea1f8c0b117938dd00a209f39ab7d6b9ac38460
Nischal Singh <84283501+nischal-it-is@users.noreply.github.com>
2021-09-13 14:55:58 +0530

    useState Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 //var user = prompt("Enter User Name");
 var user = "Nischal";
 var count = 0;
 export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
   function likeClickHandler() {
-    console.log(count);
     count += 1;
+    setLikeCounter(count);
   }
   return (
     <div className="App">
       <h1 style={{ color: "blue" }}>Welcome {user}</h1>
       <button onClick={likeClickHandler}>Like</button>
-      <h2>Yo</h2>
+      <h2>{likeCounter}</h2>
     </div>
   );
 }
