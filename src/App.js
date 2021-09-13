a54a5206b231760fe3dc4f580c96c5d2b9607541
Nischal Singh <84283501+nischal-it-is@users.noreply.github.com>
2021-09-13 14:51:18 +0530

    Button addded and onclick done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import "./styles.css";
 
 //var user = prompt("Enter User Name");
 var user = "Nischal";
+var count = 0;
 export default function App() {
+  function likeClickHandler() {
+    console.log(count);
+    count += 1;
+  }
   return (
     <div className="App">
       <h1 style={{ color: "blue" }}>Welcome {user}</h1>
+      <button onClick={likeClickHandler}>Like</button>
       <h2>Yo</h2>
     </div>
   );
