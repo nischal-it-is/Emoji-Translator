731d4f82a131b6dfebaf43902cda8d22c5bfa290
Nischal Singh <84283501+nischal-it-is@users.noreply.github.com>
2021-09-13 14:30:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+//var user = prompt("Enter User Name");
+var user = "Nischal";
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ color: "blue" }}>Welcome {user}</h1>
+      <h2>Yo</h2>
+    </div>
+  );
+}
