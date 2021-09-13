da1063fa3be1ad209410cb202cb02d05aebf661f
Nischal Singh <84283501+nischal-it-is@users.noreply.github.com>
2021-09-13 15:21:53 +0530

    Emoji meaning setting done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
 import "./styles.css";
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑":
+    "annoyance" /** add some more to show how the app now expands when there's new data */
+};
 
 //var user = prompt("Enter User Name");
 var user = "Nischal";
 var count = 0;
 export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
-  const [userInput, setUserInput] = useState(0);
-  function likeClickHandler() {
-    count += 1;
-    setLikeCounter(count);
-  }
-  function inputHandler(event) {
-    setUserInput(event.target.value);
+  // const [likeCounter, setLikeCounter] = useState(0);
+  // const [userInput, setUserInput] = useState(0);
+  // function likeClickHandler() {
+  //   count += 1;
+  //   setLikeCounter(count);
+  // }
+  const [emoji, setMeaning] = useState("");
+  function emojiHandler(event) {
+    var userInput = event.target.value;
+    var emojiMeaning = emojiDictionary[userInput];
+    console.log(emojiMeaning);
+    setMeaning(emojiMeaning);
   }
   return (
     <div className="App">
       <h1 style={{ color: "blue" }}>Welcome {user}</h1>
-      <input onChange={inputHandler}></input>
-      <div>{userInput}</div>
+      <input onChange={emojiHandler}></input>
+      {/* /* <div>{userInput}</div>
       <button onClick={likeClickHandler}>Like</button>
-      <h2>{likeCounter}</h2>
+      <h2>{likeCounter}</h2> */}
+      <h4>{emoji}</h4>
     </div>
   );
 }
